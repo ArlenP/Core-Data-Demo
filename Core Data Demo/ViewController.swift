4b650c0f000aed8efd0d399cd0caa49505be7119
ArlenP <loloss>
2017-10-23 16:37:38 -0500

    Initial Commit

---
+++ Core Data Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data Demo
+//
+//  Created by Arlen Peña Tolentino on 10/23/17.
+//  Copyright © 2017 Arlen Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
